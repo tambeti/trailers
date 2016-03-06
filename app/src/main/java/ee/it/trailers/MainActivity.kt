a1a003bfb06cc9613d4b66f16995d8c8befec572
Tambet Ingo <tingo@litl.com>
2016-03-06 11:30:07 +0200

    Handle magnet links

--- app/src/main/java/ee/it/trailers/MainActivity.kt
+++ app/src/main/java/ee/it/trailers/MainActivity.kt
@@ -1,13 +1,27 @@
 package ee.it.trailers
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import ee.it.trailers.tmdb.Movie
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), MoviesFragment.OnMovieSelected {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (intent?.action?.equals(Intent.ACTION_VIEW)?:false) {
+            val uri = intent.data
+            var app = application as MyApplication
+            var pulsar = Pulsar(this, app.httpClient)
+            pulsar.playUri(uri.toString())
+            Toast.makeText(this, R.string.magnet_sent, Toast.LENGTH_SHORT)
+                    .show()
+            finish()
+            return
+        }
+
         super.setContentView(R.layout.activity_main)
 
         setSupportActionBar(my_toolbar)
